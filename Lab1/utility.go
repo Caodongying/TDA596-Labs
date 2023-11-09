268ab4f1e13c2e216df5d785906ba8ce6054182f
Dieter <dieter.deboosere@student.kuleuven.be>
2023-11-09 09:34:33 +0100

    added utility.go

---
+++ Lab1/utility.go
@@ -0,0 +1,37 @@
+package utility
+
+import (
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+)
+
+const (
+	NetworkConn = "tcp"
+	HostConn    = "0.0.0.0"
+)
+
+var ServerPort int // can't be here!
+
+func PrintRequest(request *http.Request) {
+	reqDump, err := httputil.DumpRequest(request, true)
+	if err != nil {
+		fmt.Println("Dumping request Error:", err.Error())
+		return
+	}
+	fmt.Printf("REQUEST:\n%s", string(reqDump))
+}
+
+func SendResponse(conn net.Conn, statusCode int, responseMessage string) {
+	responseHeader := fmt.Sprintf("HTTP/1.1 %d %s\r\n", statusCode, responseMessage)
+	responseBody := fmt.Sprintf("%d %s", statusCode, responseMessage)
+	conn.Write([]byte(responseHeader))
+	conn.Write([]byte("\r\n"))
+	conn.Write([]byte(responseBody))
+}
+
+func ReleaseBufferChannel(channel chan string) {
+	temp := <-channel
+	fmt.Println(temp)
+}
